c0401df8df1f6c081af667d3c857fd7dbbc002f2
Lyamch228 <60107696+Lyamch228@users.noreply.github.com>
2020-02-20 17:30:08 +0600

    Add files via upload

---
+++ cogs/moderation.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+import discord, asyncpg
+from discord.ext import commands
+from discord import utils
+class commands(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["purge"])
+    @commands.has_permissions(manage_messages=True)
+    @commands.bot_has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int = 1):
+        await ctx.message.delete()
+        await ctx.channel.purge(limit=amount)
+        if amount == 1:
+            await ctx.send(f"Purged {amount} message", delete_after=10)
+        else:
+            await ctx.send(f"Purged {amount} messages", delete_after=10)
+            
+    @commands.command()
+    @commands.has_permissions(manage_nicknames=True)
+    @commands.bot_has_permissions(manage_nicknames=True)
+    async def setnick(self, ctx, member: discord.Member, *, nick):
+        if member == ctx.guild.owner:
+            await ctx.send("I can't edit the server owner!")
+            return
+        if len(nick) > 32:
+            await ctx.send("Nickname too long!")
+            return
+        await member.edit(nick=nick)
+        await ctx.send(f"Set nick for {member.name} to {nick}")
+
+    @commands.command()
+    @commands.has_permissions(manage_nicknames=True)
+    @commands.bot_has_permissions(manage_nicknames=True)
+    async def resetnick(self, ctx, member: discord.Member):
+        await member.edit(nick=member.name)
+
+def setup(bot):
+    bot.add_cog(commands(bot))
