78b346ac5204a3ca54805e8bbd0a3c727dd1114b
Lyamch228 <60107696+Lyamch228@users.noreply.github.com>
2020-04-19 23:45:06 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,7 +7,16 @@ import json
 
 bot = commands.Bot(command_prefix = "+")
 bot.remove_command("help")
-		
+
+@bot.command(name='cl')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+
 @bot.command()
 async  def load(ctx, extension):
 	bot.initial_extension(f'{extension}')
