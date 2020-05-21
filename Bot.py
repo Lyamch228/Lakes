6d0dec86b9faa5fed64abc27d3cc75ea92744fe1
Lyamch228 <60107696+Lyamch228@users.noreply.github.com>
2020-05-21 23:49:24 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -133,7 +133,7 @@ async def help2(ctx):
 @bot.command()
 @commands.has_permissions( administrator = True )
 async def tempmute(ctx,amount : int,member: discord.Member = None, reason = None):
-    mute_role = discord.utils.get(member.guild.roles, id = 707612817204838491, 699219397566529536) #Айди роли
+    mute_role = discord.utils.get(member.guild.roles, id = 707612817204838491) #Айди роли
     channel_log = bot.get_channel(704191405807108186) #Айди канала логов
 
     await member.add_roles( mute_role )
