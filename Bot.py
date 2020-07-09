29f6b9280de113d9237aa878059756c3a224682e
Lyamch228 <60107696+Lyamch228@users.noreply.github.com>
2020-07-09 16:09:43 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -136,7 +136,7 @@ async def help(ctx):
 	
 	embeds = [embed1, embed2, embed3, embed4]
 	msg = await ctx.send(embed = embed1)
-	page = pag(bot, msg, only=ctx.author, use_more=True, embeds=embeds, more_reactions = reactions, exit_reaction = react)
+	page = pag(bot, msg, only=ctx.author, use_more=True, embeds=embeds, more_reactions = reactions, delete_message = react)
 	await page.start();
 
 @bot.command()
