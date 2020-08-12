8377b2b0f4eab48d13387459d6129dc381fe339d
Lyamch228 <60107696+Lyamch228@users.noreply.github.com>
2020-08-13 04:51:11 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -65,10 +65,6 @@ async def teleportation(ctx, arg=None, member: discord.Member = None):
             await member.edit(voice_channel=vchannel)
 
 @bot.command()
-async def раковой(ctx):
-        await ctx.message("Раковой гей")
-
-@bot.command()
 async def ping(ctx):
     try:
         await ctx.message.delete()
