0c19f9f967fd40ead489ef5d21838f811aeffc66
Lyamch228 <60107696+Lyamch228@users.noreply.github.com>
2021-02-13 22:51:54 +0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -212,7 +212,7 @@ async def bite(ctx, member : discord.Member):
 @bot.command()
 async def hentai(ctx, member : discord.Member):
     emb.set_image(url=nekos.img('hentai'))
-    await ctx.send(embed=emb))
+    await ctx.send(embed=emb)
 
 @slap.error
 async def slap_error(ctx, error):
