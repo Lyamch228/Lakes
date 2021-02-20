ca838b45aab831040c7534a6e936fec52fbde71c
Lyamch228 <60107696+Lyamch228@users.noreply.github.com>
2021-02-20 22:54:42 +0600

    Delete utils directory

--- utils/default.py
+++
@@ -1,6 +0,0 @@
-def uptime(dt):
-    days = dt.days
-    hours, r = divmod(dt.seconds, 3600)
-    minutes, seconds = divmod(r, 60)
-
-    return f"• Days: `{days}` | Hours: `{hours}` | Minutes: `{minutes}` | Seconds: `{seconds}`"
